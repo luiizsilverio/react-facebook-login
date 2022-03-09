2f3caa42b362b2209632eadf0ccc6d584de70de9
luiizsilverio <luiiz.silverio@gmail.com>
2022-03-08 21:20:02 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import FacebookLogin from 'react-facebook-login';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isLogged, setIsLogged] = useState(false)
+  const [user, setUser] = useState({
+    userID: "",
+    email: "",
+    picture: ""    
+  })
+
+  const responseFacebook = (response) => {
+    const { userID, name, email, picture } = response
+
+    console.log(response)
+    setIsLogged(!!userID)
+
+    setUser({
+      userID,
+      name,
+      email,
+      picture: picture.data.url
+    })
+  }
+
+  const componentClicked = () => {
+    // alert('Você está logado.')
+  }
+
+	return (
+		<div className="container">
+      {
+        !isLogged ?
+          <FacebookLogin
+            appId={process.env.REACT_APP_CLIENT_ID}
+            autoLoad={false}
+            fields="name,email,picture"
+            textButton="Entrar com Facebook"
+            icon="fa-facebook"
+            cssClass="my-facebook-button-class"
+            onClick={componentClicked}
+            callback={responseFacebook} 
+          />
+        :      
+          <div className="center">
+            <h1>Dados do usuário</h1>
+            <img src={user.picture} alt={user.name} className="profile" />
+            <p>Nome: {user.name}</p>
+            <p>E-mail: {user.email}</p>
+          </div>
+      }
+		</div>
+	);
 }
 
 export default App;
